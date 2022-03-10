455d7024ae7bdefca530328b631176f21e46b4f1
Mahe1998 <87573259+Mahe1998@users.noreply.github.com>
2022-03-10 22:42:33 +0530

    React components completed.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+//"./Heading" can be used in place of "./Heading.jsx".
+import Heading from "./Components/Heading.jsx";
+import List from "./Components/List";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+//"./Heading" can be used in place of "./Heading.jsx".
+import Heading from "./Heading.jsx";
+import List from "./List";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
